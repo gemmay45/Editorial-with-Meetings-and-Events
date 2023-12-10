1ad311c776e9f12aa5795756fe4a6089ab03cc89
admin admin <evaladmin@example.com>
2023-12-10 16:08:25 +0800

    "User admin wrote content /scripts/pages/dining-landing.groovy"

--- scripts/pages/dining-landing.groovy
+++ scripts/pages/dining-landing.groovy
@@ -15,5 +15,6 @@ def maxTenants = contentModel.maxTenants_i
 def searchHelper = new TenantSearchHelper(searchClient, urlTransformationService)
 def tenants = searchHelper.searchTenants(false, null, segment, 0, maxTenants)
 
+templateModel.count = tenants.size()
 templateModel.tenants = tenants
 /*templateModel.categories = categories*/
