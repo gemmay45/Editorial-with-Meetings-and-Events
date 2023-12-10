c58921df18ad0153bb409b4b83419c47aa2bfb54
gemmay45 <gemma.yung@gmail.com>
2023-12-10 17:33:15 +0800

    commit

--- scripts/classes/org/craftercms/sites/editorial/tenantSearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/tenantSearchHelper.groovy
@@ -29,7 +29,7 @@ import org.craftercms.search.opensearch.client.OpenSearchClientWrapper
 
 class TenantSearchHelper {
 
-  static final String TENANT_CONTENT_TYPE = "/page/tenant"
+  static final String TENANT_CONTENT_TYPE = "/page/dining"
   static final List<String> TENANT_SEARCH_FIELDS = [
     'name_s^1.5',
     'sections_o.item.section_html^1.0'
@@ -230,20 +230,9 @@ class TenantSearchHelper {
         tenant.path = doc.localId
         tenant.storeUrl = doc.localId
         tenant.title = doc.name_s
-        tenant.summary = doc.summary_t
         tenant.url = urlTransformationService.transform("storeUrlToRenderUrl", doc.localId)
-        tenant.image = doc.images_o.item[0].image_s
-        tenant.tagline = doc.tagline_s
-        
-        
-        def categories = doc.categories_o.item.value_smv
-
-        def catCount = (doc.categories_o.item[1]) ? doc.categories_o.item.value_smv.size() : 1
-        if (catCount==1) {
-            tenant.categories = [categories]
-        }else {
-            tenant.categories = categories
-        }
+        /*tenant.image = doc.teaserImages_s
+        tenant.teaserDescription_html = doc.teaserDescription_html*/
         
         tenants << tenant
       }
