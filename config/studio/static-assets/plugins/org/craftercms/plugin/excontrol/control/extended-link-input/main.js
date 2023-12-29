a708f0d2ffe44325964ccdbcff596c31c8694bf4
admin admin <evaladmin@example.com>
2023-12-29 13:07:36 +0800

    Copy plugin org.craftercms.plugin.excontrol 1.0.0

--- config/studio/static-assets/plugins/org/craftercms/plugin/excontrol/control/extended-link-input/main.js
+++ config/studio/static-assets/plugins/org/craftercms/plugin/excontrol/control/extended-link-input/main.js
@@ -355,7 +355,7 @@ YAHOO.extend(CStudioForms.Controls.extendedLinkInput, CStudioForms.CStudioFormFi
     newdiv.innerHTML =
       '<div id="panel1" class="yui-module yui-overlay yui-panel" style="visibility: inherit;width: 480px; ">' +
       '<div role="dialog" aria-modal="true" tabindex="-1" class="tox-dialog" aria-labelledby="dialog-label_362831062241703822078255" aria-describedby="dialog-describe_8249867402251703822078255" style="position: relative;">' +
-      '<div class="tox-dialog__body-content">' + 
+      '<div class="submitxformscroll">' + 
       '<div class="tox-form">' +
       '<div class="tox-form__group" aria-disabled="false">' +
       '<label class="tox-label" for="form-field_591596152161703822078253">URL</label>' +
