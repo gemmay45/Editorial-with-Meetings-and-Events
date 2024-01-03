86fb81c9cdf9d000058dffde2d5cc2d027d49e12
admin admin <evaladmin@example.com>
2024-01-03 14:22:01 +0800

    Copy plugin org.craftercms.plugin.excontrol 1.0.0

--- config/studio/static-assets/plugins/org/craftercms/plugin/excontrol/control/extended-link-input/main.js
+++ config/studio/static-assets/plugins/org/craftercms/plugin/excontrol/control/extended-link-input/main.js
@@ -254,10 +254,8 @@ YAHOO.extend(CStudioForms.Controls.extendedLinkInput, CStudioForms.CStudioFormFi
     this.displayTxtEl.id = "displayTxt"
     this.displayTxtEl.disabled = true;
     this.displayTxtEl.setAttribute('style','padding: 5px 10px; width: 100%;display: flex;background: #fafafa;border: 1px solid #ced4da;border-radius: 0 0 4px 4px;align-items: center;');
-    var displayTxtSpanEl = document.createElement('span');
-    displayTxtSpanEl.value = "&nbsp;"
-    //    var txt = document.createTextNode("&lt;span> </span>");
-    this.displayTxtEl.appendChild(displayTxtSpanEl)
+    var txt = document.createTextNode("-");
+    this.displayTxtEl.appendChild(txt)
     inputContainerDivEl.appendChild(displayTxtEl);
 
     YAHOO.util.Event.on(
